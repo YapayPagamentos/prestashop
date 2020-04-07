965fb6288e019b3f4c8c885df4e83cd113a184e5
abnava <ana.nava@yapay.com.br>
2020-04-07 16:32:22 -0300

    Tag PrestaShop2.2

--- yapay_prestashop/traycheckout/traycheckout.php
+++ yapay_prestashop/traycheckout/traycheckout.php
@@ -384,8 +384,7 @@ class traycheckout extends PaymentModule
 			'shipping_type' 	=> $shipping_type,		
 			'total_paid' 	=> $total_paid,
 			'url_notification' => $url_notification,
-			'free' => "PRESTASHOP_v2.1",
-			'max_split_transaction' => "6",
+			'free' => "PRESTASHOP_v2.2",
 			'available_payment_methods' => "2, 3, 4, 5, 15, 16, 18, 19, 20, 25, 6"
 		));
 		
